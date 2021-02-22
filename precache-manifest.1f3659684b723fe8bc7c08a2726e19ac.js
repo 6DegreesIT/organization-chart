fbae9a714c388c5d75a6c2280c5efffc4b4bc3e1
Vibhor Mungee <vibhor.mungee@6degreesit.com>
2021-02-22 19:51:00 +0530

    Updates

---
+++ precache-manifest.1f3659684b723fe8bc7c08a2726e19ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ca3eb20afb76d96723153a0aa5a6d3",
+    "url": "/react-org-chart/index.html"
+  },
+  {
+    "revision": "06501733e24e1cbfa559",
+    "url": "/react-org-chart/static/css/main.047fb8b0.chunk.css"
+  },
+  {
+    "revision": "2cb1759c305d3e4ff7b8",
+    "url": "/react-org-chart/static/js/2.ec6cc475.chunk.js"
+  },
+  {
+    "revision": "44af8894b5b819badfc92b5a6c65eeb0",
+    "url": "/react-org-chart/static/js/2.ec6cc475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06501733e24e1cbfa559",
+    "url": "/react-org-chart/static/js/main.37df34ed.chunk.js"
+  },
+  {
+    "revision": "faf929761cfdd5e81a7f",
+    "url": "/react-org-chart/static/js/runtime-main.36560262.js"
+  },
+  {
+    "revision": "f8399445e0f391bfe9059f4b992b3657",
+    "url": "/react-org-chart/static/media/avatar-personnel.f8399445.svg"
+  }
+]);
